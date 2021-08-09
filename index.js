e543145e7c0345abb4fef8dfcfc04d91deed8c8e
Dao Trung Kien <trung-kien.dao@mica.edu.vn>
2021-08-09 13:38:20 +0700

    Change uglify-js to uglify-es

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var Fs = require('fs'),
     Path = require('path'),
-    UglifyJS = require('uglify-js');
+    UglifyJS = require('uglify-es');
 
 /**
  * Checks if a path exists and is a file (not a directory), without throwing any error
